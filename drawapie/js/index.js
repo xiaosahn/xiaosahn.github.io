65921ac243d1a34be92306fdaf2a0d4178a10466
xiaosahn <98068993+xiaosahn@users.noreply.github.com>
2023-12-25 00:02:18 +0800

    yugao

---
+++ drawapie/js/index.js
@@ -0,0 +1,36 @@
+/** 欢迎使用guiPlan,以下代码均由guiPlan软件自动生成，如有疑问添加添加作者微信guiplan或加入学习交流qq群711964722  网站htpp://www.guiplan.com **/
+// interfaceCode
+var App = {
+    name: '',
+    mounted() {
+
+        // defaultLoad
+        // drawapieindexhtmlMountedStart
+
+        // drawapieindexhtmlMountedEnd
+
+    },
+    components: {},
+    data() {
+        return {
+            // insertData
+            // drawapieindexhtmlDataStart
+
+            // drawapieindexhtmlDataEnd
+
+        }
+    },
+    methods: {
+        default () {},
+        // insertMethod
+        // drawapieindexhtmlMethodStart
+
+        // drawapieindexhtmlMethodEnd
+
+        // methodsCode
+
+    },
+    computed: {}
+}
+const app = Vue.createApp(App);
+app.mount("#defaultId1");
