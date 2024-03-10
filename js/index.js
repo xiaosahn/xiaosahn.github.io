f5836b32da0bb3dce33f146e1c702f1a3ca8aa4e
xiaosahn <98068993+xiaosahn@users.noreply.github.com>
2024-03-10 17:34:01 +0800

    网站更新2
    网站更新2

---
+++ js/index.js
@@ -0,0 +1,36 @@
+/** 欢迎使用guiPlan,以下代码均由guiPlan软件自动生成，如有疑问添加添加作者微信guiplan或加入学习交流qq群711964722  网站htpp://www.guiplan.com **/
+// interfaceCode
+var App = {
+    name: '',
+    mounted() {
+
+        // defaultLoad
+        // websiteindexhtmlMountedStart
+
+        // websiteindexhtmlMountedEnd
+
+    },
+    components: {},
+    data() {
+        return {
+            // insertData
+            // websiteindexhtmlDataStart
+
+            // websiteindexhtmlDataEnd
+
+        }
+    },
+    methods: {
+        default () {},
+        // insertMethod
+        // websiteindexhtmlMethodStart
+
+        // websiteindexhtmlMethodEnd
+
+        // methodsCode
+
+    },
+    computed: {}
+}
+const app = Vue.createApp(App);
+app.mount("#defaultId1");
