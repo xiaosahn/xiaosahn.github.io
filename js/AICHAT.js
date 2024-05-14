3c88154d94e0139f55fd705a8ab6ad1bc68519a2
xiaosahn <98068993+xiaosahn@users.noreply.github.com>
2024-05-14 23:14:36 +0800

    gx

---
+++ js/AICHAT.js
@@ -0,0 +1,36 @@
+/** 欢迎使用guiPlan,以下代码均由guiPlan软件自动生成，如有疑问添加添加作者微信guiplan或加入学习交流qq群711964722  网站htpp://www.guiplan.com **/
+// interfaceCode
+var App = {
+    name: '',
+    mounted() {
+
+        // defaultLoad
+        // websiteAICHAThtmlMountedStart
+
+        // websiteAICHAThtmlMountedEnd
+
+    },
+    components: {},
+    data() {
+        return {
+            // insertData
+            // websiteAICHAThtmlDataStart
+
+            // websiteAICHAThtmlDataEnd
+
+        }
+    },
+    methods: {
+        default () {},
+        // insertMethod
+        // websiteAICHAThtmlMethodStart
+
+        // websiteAICHAThtmlMethodEnd
+
+        // methodsCode
+
+    },
+    computed: {}
+}
+const app = Vue.createApp(App);
+app.mount("#defaultId1");
